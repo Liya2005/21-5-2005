2ef7f04aef115a49c696bcbf36b2550e6c2f23cd
Ellakiyamurugesh <147843966+Liya2005@users.noreply.github.com>
2024-05-21 20:21:44 +0530

    Update

---
+++ number between.py
@@ -0,0 +1,9 @@
+def print_numbers_between(a, b):
+    for num in range(a, b+1):
+        print(num)
+
+# Sample input: values of a and b
+a = 5
+b = 10
+print("Numbers between", a, "and", b, "inclusive:")
+print_numbers_between(a, b)
